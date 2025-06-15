ea24e89870fb0eddd28c670821fba913096da5b9
Sebas-RS <sebas.r.s18@gmail.com>
2025-06-15 11:45:03 +0000

    Todo list terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
+
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	const [nuevaTarea, setNuevaTarea] = useState ("Ejemplo");
+    const [listaTarea, setListaTarea] = useState (["Hacer la cama", "Cepillarme los dientes"]);
 
-export default Home;
+    const handleClick = () => {
+		if (nuevaTarea !== ""){
+			setListaTarea([...listaTarea, nuevaTarea]);
+			setNuevaTarea("");
+		}
+	};
+	//agregar una nueva tarea
+
+	const borrarTarea = (index) => {
+		
+		const listaNueva = listaTarea.filter((tarea, i) => i !== index);
+			setListaTarea(listaNueva);	
+	}
+
+	const handleChange = (event) => {
+		setNuevaTarea(event.target.value)	
+	}
+	//mantiene actualizado
+		return(
+			<div className = "text-center">
+				<h1 className="text-center mt-5">Todo List
+				</h1>
+					<div className="container w-50 mx-auto">
+						<div className="d-flex gap-2">
+							<input type="text"
+							className="form-control" 
+							onChange={handleChange}
+							onKeyDown={(e) => {
+							  if (e.key === "Enter"){
+								 handleClick ();
+							}
+                            }}
+							placeholder="Agregar tarea"
+							/>
+							
+					    </div>
+				
+						<ul className="list-group">
+						  {listaTarea.map((tarea, index) => (
+						    <li 
+							  className="list-group-item tarea-item d-flex justify-content-between align-items-center"
+						      key={index}>{tarea} <button 
+						     onClick={() => borrarTarea(index)}
+						     className="btn btn btn-sm tarea-delete-btn">x</button>
+						</li>
+						))}
+						</ul>
+						</div>
+			</div>
+		)
+	}
+
+	export default Home;
